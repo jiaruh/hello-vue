c6eef6b394f24076fb704cc1f166606daa52ef85
huangjiaru <huangjiaru@37.com>
2020-05-11 17:33:30 +0800

    update home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2> from: {{ author }} </h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -39,6 +40,7 @@ export default Vue.extend({
   name: 'HelloWorld',
   props: {
     msg: String,
+    author: String
   },
 });
 </script>
